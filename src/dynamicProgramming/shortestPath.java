32d84b40ecc793390e48c44b7a66c66658f04c20
Akanksha Kanjolia <akanksha.kanjolia@myntra.com>
2020-10-22 12:15:39 +0530

    sliding windowQuestion

---
+++ src/dynamicProgramming/shortestPath.java
@@ -0,0 +1,19 @@
+//package dynamicProgramming;
+//
+//public class shortestPath {
+//
+//    public static int smallestPath(int m, int n){
+//        int[][] dp = new int[m + 1][n + 1];
+//
+//        if(n ==0||m ==0){
+//            return 2;
+//        }
+//        if(n ==1||m ==1){
+//            return 0;
+//        }
+//        for(int i= 1 ; i<m; i++){
+//            for(int j = 1; j<n; j++){
+//            }
+//        }
+//    }
+//}
